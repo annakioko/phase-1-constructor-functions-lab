7588f1f847d6ff6a7cd936dad76ca1bbf75ca2cb
Anna Kioko <annakioko00@gmail.com>
2024-01-24 16:14:02 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,20 @@
+function scooter(year, color, model) {
+    this.year = year;
+    this.color = color;
+    this.model = model;
+  }
+  
+  
+  function driver(name, age, experience) {
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+  }
+  
+  
+  function pickUpLocation(address, city) {
+    this.address = address;
+    this.city = city;
+  }
+  
+  
